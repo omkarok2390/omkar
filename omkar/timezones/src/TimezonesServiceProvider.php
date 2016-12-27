1da4a8e8cfbc70a3a0870940bb0f0fdc5afadd54
omkar <omkar.kamble@yuktaMedia.com>
2016-12-27 18:34:26 +0530

    test package

---
+++ omkar/timezones/src/TimezonesServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace omkar\timezones;
+
+use Illuminate\Support\ServiceProvider;
+
+class TimezonesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+      $this->loadViewsFrom(__DIR__.'/views', 'timezones');
+      $this->publishes([
+        __DIR__.'/views' => base_path('resources/views/omkar/timezones'),
+    ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+      include __DIR__.'/routes.php';
+      $this->app->make('omkar\Timezones\TimezonesController');
+    }
+}
